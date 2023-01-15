ba4bc2ae4c48606d62a52135d9fe2b02144de8e7
ManuelV85 <manuel.villater@gmail.com>
2023-01-15 16:25:34 +0000

    new branch developer

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,32 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from random import randint
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+class users_structure:
+    def __init__(self, surname):
+        self.surname = surname
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        self.users = [{
+            "name/WS_name": "Gokubike",
+            "last_name": "",
+            "e-mail": "vegita@vegetables.com",
+            "password": "gar4gasd2",
+            "addres": "somewhere",
+            "id": self.generate_id()
+        }
+        {
+            "name/WS_name": "Vegetta",
+            "last_name": "Sayajin",
+            "e-mail": "vegita@vegetables.com",
+            "password": "gar4gasd2",
+            "addres": "somewhere",
+            "id": self.generate_id()
 
-    return jsonify(response_body), 200
+        }]
+
+        def _generate_id():
+            return randint ( 0,9999999999)
