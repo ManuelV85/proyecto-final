9750f083640d9281f00b19eff4389c07f9f068fc
ManuelV85 <manuel.villater@gmail.com>
2023-01-25 23:41:29 +0000

    se cambia el nombre

--- src/front/js/component/Logo.js
+++ src/front/js/component/Logo.js
@@ -4,12 +4,14 @@ import "../../styles/Logo.css";
 export const Logo = () => {
   return (
     <div className="logo">
-      <span className="M">M</span>
-      <span className="Y">Y</span>
+      <span className="i">i</span>
+      <span className="P">P</span>
+      <span className="r">r</span>
+      <span className="o">o</span>
       <span className="B">B</span>
-      <span className="I">I</span>
-      <span className="K">K</span>
-      <span className="E">E</span>
+      <span className="i">i</span>
+      <span className="k">k</span>
+      <span className="e">e</span>
     </div>
   );
 };
